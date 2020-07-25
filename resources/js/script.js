a4cfbed4953f7ce9a83567308ba92e817255533b
DattaAnupam <aanupamdatta.1995@gmail.com>
2020-07-25 13:27:09 +0530

    Added mobile nav and made the main nav responsive to mobile

--- resources/js/script.js
+++ resources/js/script.js
@@ -14,15 +14,22 @@ $(document).ready(function()
     });
 
     /* --- Scroll on button click --- */
-    $('.js--scroll-to-plan').click(function() {
-        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top},1000);
+    $('.js--scroll-to-plan')
+    .click(function() {
+        $('html, body')
+        .animate({scrollTop: $('.js--section-plans').offset().top},1000);
     });
-    $('.js--scroll-to-features').click(function() {
-        $('html, body').animate({scrollTop: $('.js--section-features').offset().top},1000);
+    $('.js--scroll-to-features')
+    .click(function() {
+        $('html, body')
+        .animate({scrollTop: $('.js--section-features').offset().top},1000);
     });
 
     /* --- Scroll for Nav Bar --- */ 
-    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
+    $('a[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
         if (
             location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
             && 
@@ -33,19 +40,59 @@ $(document).ready(function()
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
                 event.preventDefault();
-                $('html, body').animate({scrollTop: target.offset().top}, 1000, function() {
+                $('html, body')
+                .animate({scrollTop: target.offset().top}, 1000, function() {
                     var $target = $(target);
                     $target.focus();
                     if ($target.is(":focus")) { 
                         return false;
                     }
                     else {
-                        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                        $target.attr('tabindex','-1'); 
                         $target.focus();
                     }
                 });
             }
         }
+    });
+
+    /* --- Animation on click --- */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animate__animated  animate__fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animate__animated animate__flash');
+    }, {
+        offset: '50%'
+    });
+
+    /* --- Mobile Nav --- */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        nav.slideToggle(200);
+        if(icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        }
+        else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round'); 
+        }
+
+        ion-close-round
     });
 
 });
